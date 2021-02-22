87464d718a36964f017169a1dca874f5b4a5a7d5
daksha <dakshaaeer@gmail.com>
2021-02-22 19:23:33 +0530

    fix: Correct port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/div', (req, res) => {
     res.send('divident is ' + c)
 })
 
-const PORT = process.env.PORT | 3000
-app.listen(3000, () => {
+const PORT = process.env.PORT | 5000
+app.listen(PORT, () => {
     console.log('Started')
 })
